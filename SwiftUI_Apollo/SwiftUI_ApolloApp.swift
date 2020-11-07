0acbfdb875a635be20c3b6cac3d2b0fea4b55b26
Pavlo Vorobiov <>
2020-11-07 09:00:42 +0200

    Initial commit

---
+++ SwiftUI_Apollo/SwiftUI_ApolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ApolloApp.swift
+//  SwiftUI_Apollo
+//
+//  Created by Pavlo Vorobiov on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ApolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
